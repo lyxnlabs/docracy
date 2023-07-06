df9b1fbc217f7e342b75fdcb95ce982cfd047837
Mohammed Soharab S <soharab@quinta.co.in>
2023-07-06 23:40:57 +0530

    Styling and Voting Logic

--- src/components/Polls.jsx
+++ src/components/Polls.jsx
@@ -7,6 +7,7 @@ import {
   Button,
   useMediaQuery,
   Typography,
+  Alert,
 } from "@mui/material";
 import avatar1 from "../assets/img/avatars/avatar1.png";
 import avatar2 from "../assets/img/avatars/avatar2.png";
@@ -19,33 +20,31 @@ import avatar8 from "../assets/img/avatars/avatar8.png";
 import avatar9 from "../assets/img/avatars/avatar9.png";
 
 import CloudDoneIcon from "@mui/icons-material/CloudDone";
+import { TbSignRightFilled } from "react-icons/tb";
 const posts = [
   // Example candidate data
-  { id: 1, name: "Secretary 1" },
-  { id: 2, name: "Treasurer 2" },
-  { id: 3, name: "President 3" },
-  { id: 4, name: "President 4" },
-  { id: 5, name: "President 5" },
+  { id: 1, name: "Secretary " },
+  { id: 2, name: "Treasurer " },
+  { id: 3, name: "President " },
+  { id: 4, name: "Joint Secretary" },
+  { id: 5, name: "Joint Secretary " },
   // ... add more candidates
 ];
 
 const candidates = [
   { id: 1, name: "Candidate 1", image: avatar1, postId: 1 }, // Assign postId to candidates
   { id: 2, name: "Candidate 2", image: avatar2, postId: 1 },
-  { id: 3, name: "Candidate 3", image: avatar3, postId: 1 },
-  { id: 4, name: "Candidate 4", image: avatar4, postId: 1 },
-  { id: 5, name: "Candidate 5", image: avatar5, postId: 1 },
-  { id: 6, name: "Candidate 6", image: avatar6, postId: 2 },
-  { id: 7, name: "Candidate 7", image: avatar7, postId: 2 },
-  { id: 8, name: "Candidate 8", image: avatar8, postId: 2 },
-  { id: 9, name: "Candidate 9", image: avatar9, postId: 3 },
-  { id: 10, name: "Candidate 10", image: avatar8, postId: 4 },
-  { id: 11, name: "Candidate 11", image: avatar9, postId: 4 },
+  { id: 3, name: "Candidate 3", image: avatar3, postId: 2 },
+  { id: 4, name: "Candidate 4", image: avatar4, postId: 2 },
+  { id: 5, name: "Candidate 5", image: avatar5, postId: 2 },
+  { id: 6, name: "Candidate 6", image: avatar6, postId: 3 },
+  { id: 7, name: "Candidate 7", image: avatar7, postId: 3 },
+  { id: 8, name: "Candidate 8", image: avatar8, postId: 4 },
+  { id: 9, name: "Candidate 9", image: avatar9, postId: 4 },
+  { id: 10, name: "Candidate 10", image: avatar8, postId: 5 },
+  { id: 11, name: "Candidate 11", image: avatar9, postId: 5 },
   // ... add more candidates
 ];
-
-
-
 
 const Polls = () => {
   const [selectedCandidate, setSelectedCandidate] = useState(null);
@@ -56,7 +55,17 @@ const Polls = () => {
 
   const handleCandidateClick = (candidate, candidate_id, post_id) => {
     setSelectedCandidate(candidate);
-    setCasts([...casts, { post_id: post_id, candidate_id: candidate_id }]);
+    let index = casts.findIndex(function (item) {
+      return item.post_id === post_id;
+    });
+    if (index !== -1) {
+      // If post_id exists, replace the old item with the new one
+      casts[index] = { post_id: post_id, candidate_id: candidate_id };
+    } else {
+      // If post_id doesn't exist, add the new item to the casts array
+      casts.push({ post_id: post_id, candidate_id: candidate_id });
+    }
+    console.log(casts);
   };
   const cardRef = useRef(null); // Ref for the Card component
 
@@ -71,7 +80,9 @@ const Polls = () => {
       }, 300);
     }
   };
-  const filteredCandidates = candidates.filter(candidate => candidate.postId === posts[currentPostIndex].id);
+  const filteredCandidates = candidates.filter(
+    (candidate) => candidate.postId === posts[currentPostIndex].id
+  );
 
   const handleBack = () => {
     if (currentPostIndex > 0) {
@@ -85,10 +96,6 @@ const Polls = () => {
   };
 
   const handleReviewAndSubmit = () => {
-    // Construct the JSON object containing user selections
-    const userSelections = {
-      [posts[currentPostIndex].name]: selectedCandidate.name,
-    };
     console.log(JSON.stringify(casts));
   };
 
@@ -107,9 +114,11 @@ const Polls = () => {
       }}
     >
       <CardHeader title={"Vote for " + posts[currentPostIndex].name} />
+      {!(casts.length >= (currentPostIndex+1)) && <Alert sx ={{ml:2, mr:2}} severity="error">Please select a candidate</Alert>}
       <CardContent>
         <Grid container spacing={2}>
-        {filteredCandidates.map((candidate) => (
+          {filteredCandidates.map((candidate) => (
+            
             <Grid item xs={6} sm={4} key={candidate.id}>
               <Card
                 onClick={() =>
@@ -122,12 +131,15 @@ const Polls = () => {
                 sx={{
                   cursor: "pointer",
                   backgroundColor:
-                    selectedCandidate?.id === candidate.id
+                    casts[currentPostIndex]?.candidate_id === candidate.id
                       ? "#e0e0e0"
                       : "white",
                   opacity: showCard ? 1 : 0,
                   transition: "opacity 0.3s",
-                  border: "1px dotted black",
+                  border:
+                  casts[currentPostIndex]?.candidate_id === candidate.id
+                      ? "2px solid black"
+                      : "1px dotted black",
                   width: "100%",
                 }}
               >
@@ -139,17 +151,17 @@ const Polls = () => {
                   />
                   <Typography variant="body2" align="center">
                     {candidate.name}
+                    {selectedCandidate?.id === candidate?.id && ( // Conditionally render tick mark
+                      <TbSignRightFilled
+                        style={{
+                          position: "absolute",
+                          top: 10,
+                          right: 10,
+                          color: "#fff",
+                        }}
+                      />
+                    )}
                   </Typography>
-                  {selectedCandidate?.id === candidate.id && ( // Conditionally render tick mark
-                    <CloudDoneIcon
-                      sx={{
-                        position: "absolute",
-                        top: 10,
-                        right: 10,
-                        color: "black",
-                      }}
-                    />
-                  )}
                 </CardContent>
               </Card>
             </Grid>
@@ -168,9 +180,9 @@ const Polls = () => {
             onClick={handleBack}
             disabled={currentPostIndex === 0}
             sx={{
-              backgroundColor: "#02ad80",
+              backgroundColor: "#000",
               "&:hover": {
-                backgroundColor: "#035e46",
+                backgroundColor: "#000",
                 color: "#fff",
               },
             }}
@@ -180,11 +192,12 @@ const Polls = () => {
           {!isLastPost ? (
             <Button
               variant="contained"
+              disabled = {!(casts.length >= (currentPostIndex+1))}
               onClick={handleNext}
               sx={{
-                backgroundColor: "#e86851",
+                backgroundColor: "#000",
                 "&:hover": {
-                  backgroundColor: "#873526",
+                  backgroundColor: "#000",
                   color: "#fff",
                 },
               }}
@@ -195,11 +208,11 @@ const Polls = () => {
             <Button
               variant="contained"
               onClick={handleReviewAndSubmit}
-              disabled={!selectedCandidate}
+              disabled={!(casts.length === 5)}
               sx={{
-                backgroundColor: "#e86851",
+                backgroundColor: "#000",
                 "&:hover": {
-                  backgroundColor: "#873526",
+                  backgroundColor: "#000",
                   color: "#fff",
                 },
               }}
